97ef59e672c076bfe491644550789a1f690f085e
Tokivaiya64 <sharifulislamtoki@gmail.com>
2021-02-17 22:33:57 -0800

    Initial commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL='home' 
+LOGOUT_REDIRECT_URL='home'
